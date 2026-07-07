c6b423170d3195bcdd5e0baa52e7bdb4bfbf4753
Egor <bonkegor@gmail.com>
2026-07-07 21:51:17 +0300

    feat: add redis initialization in lifespan

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from app.routers import user, search
+from app.lifespan import lifespan
 
-app = FastAPI()
 
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(search.router)
 app.include_router(user.router)
