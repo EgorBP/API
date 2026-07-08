4fdd2307ad33e376922150e6fea1ac3dbb9a58a4
Egor <bonkegor@gmail.com>
2026-07-08 16:19:06 +0300

    feat: create centralized redis connection methods

--- app/redis.py
+++ app/redis.py
@@ -1,12 +1,26 @@
-import redis.asyncio as redis
+from redis.asyncio import ConnectionPool, Redis
+from fastapi import Request
 
 
-redis_client = redis.Redis(
-    host="localhost",
-    port=6379,
-    decode_responses=True
-)
+async def create_redis_client() -> Redis:
+    """Создает пул и возвращает клиент Redis."""
+    pool = ConnectionPool(
+        host="localhost",
+        port=6379,
+        decode_responses=True,
+        max_connections=20
+    )
+    return Redis(connection_pool=pool)
 
 
-async def get_redis():
-    return redis_client
+async def close_redis_client(redis: Redis):
+    """Правильно закрывает клиент и пул соединений."""
+    pool = redis.connection_pool
+    await redis.aclose()
+    if pool:
+        await pool.disconnect()
+
+
+async def get_redis(request: Request) -> Redis:
+    """Зависимость (Dependency) для получения Redis в эндпоинтах."""
+    return request.app.state.redis
