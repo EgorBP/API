c1879530c6d61588c2e642ab2448b91fe95969b8
Egor <bonkegor@gmail.com>
2026-07-08 17:36:40 +0300

    feat: update API response codes

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException, Query, status
 from app.schemas import GifOut, GifUpdate, Successful
 from app.database import get_db
 from app.services import get_user_gifs_with_tags, set_new_user_tags_on_gif, get_all_user_tags, delete_user_gif_tags
@@ -36,7 +36,7 @@ async def get_gif(
     return data
 
 
-@router.put('/{tg_user_id}/gif/{tg_gif_id}', response_model=Successful)
+@router.put('/{tg_user_id}/gif/{tg_gif_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def update_gif_tags(
         tg_user_id: int,
         tg_gif_id: str,
@@ -50,16 +50,14 @@ async def update_gif_tags(
     - **tg_gif_id**: идентификатор GIF в Telegram
     - **gif_data**: список новых тегов
 
-    **Returns:**
-    Объект `Successful`:
-    - **successful**: bool — всегда `true`, если операция прошла успешно
+    **Returns:** HTTP 204 если операция прошла успешно
     """
     await set_new_user_tags_on_gif(db, tg_user_id, tg_gif_id, gif_data.tags)
     # return get_user_gifs_with_tags(db, tg_id=tg_user_id, tg_gifs_id=tg_gif_id)['gifs_data'][0]
-    return Successful()
+    return
 
 
-@router.delete('/{tg_user_id}/gif/{gif_id}', response_model=Successful)
+@router.delete('/{tg_user_id}/gif/{gif_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_gif_tags(
         tg_user_id: int,
         gif_id: str,
@@ -77,9 +75,7 @@ async def delete_gif_tags(
 
         Если не передано ничего выбирается вариант tg
 
-    **Returns:**
-    Объект `Successful`:
-    - **successful**: bool — всегда `true`, если удаление прошло успешно
+    **Returns:** HTTP 204 если операция прошла успешно
     """
     result = await delete_user_gif_tags(
         async_session=db,
@@ -90,7 +86,7 @@ async def delete_gif_tags(
     if not result:
         raise HTTPException(status_code=404, detail="Instances not found")
 
-    return Successful()
+    return
 
 
 @router.get('/{tg_user_id}/tags', response_model=list[str])
