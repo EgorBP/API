29810b979ecce446fad68ec034d74a579d1517f8
Egor <bonkegor@gmail.com>
2026-07-09 22:38:22 +0300

    feat: create new crud method for UserGifTagCRUD what can return joined data

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -5,6 +5,7 @@ from app import settings
 
 api_key_header = APIKeyHeader(name="X-Secret-Key", auto_error=True)
 
+
 async def verify_secret_key(key: str = Security(api_key_header)):
     """Сверяет ключ из заголовка с ключом из config.py"""
     if key != settings.BOT_API_SECRET:
